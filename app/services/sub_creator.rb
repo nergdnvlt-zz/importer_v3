def3c8387409f6fa8131aff02b8c11a97b961d6b
nergdnvlt <tylerlundgren@yahoo.com>
2020-12-06 18:54:12 -0700

    Have added bulk import, drastically reduced load time.

--- app/services/sub_creator.rb
+++ app/services/sub_creator.rb
@@ -1,10 +1,28 @@
 module SubCreator
   def save_am_subs
+    subs = @am_subs.map!{ |fields| AmericanSubscription.new(fields) }
+    results = AmericanSubscription.import subs, validate: false
+
+    if !results.failed_instances.empty?
+      binding.pry
+    end
   end
 
   def save_subs
+    subs = @subs.map!{ |fields| Subscription.new(fields) }
+    results = Subscription.import(subs)
+
+    if !results.failed_instances.empty?
+      binding.pry
+    end
   end
 
   def save_invalid_subs
+    subs = @invalid_subs.map!{ |fields| InvalidSubscription.new(fields) }
+    results = InvalidSubscription.import(subs)
+
+    if !results.failed_instances.empty?
+      binding.pry
+    end
   end
 end
